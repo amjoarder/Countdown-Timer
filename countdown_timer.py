185502c9803a8ca40e352b588903db20abb3555f
Ajmain Muhtady Joarder <fahimajmain@gmail.com>
2024-11-17 23:09:00 +0600

    second commit

--- countdown_timer.py
+++ countdown_timer.py
@@ -1,29 +1,14 @@
-import time  # Importing the time module for the sleep function
+import time
 
-# Function to perform the countdown
 def countdown(t):
-    # Loop until the timer reaches zero
     while t:
-        # Calculate minutes and seconds from the total time
-        mins, secs = divmod(t, 60)  
-        
-        # Format the timer as MM:SS with leading zeros
-        timer = '{:02d}:{:02d}'.format(mins, secs)  
-        
-        # Print the timer on the same line and overwrite the previous output
-        print(timer, end="\r")  
-        
-        # Wait for 1 second before continuing the loop
-        time.sleep(1)  
-        
-        # Decrease the timer by 1 second
-        t -= 1  
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
     
-    # Notify the user when the countdown is complete
-    print('Time Completed!')  
+    print('Time Completed!')
 
-# Prompt the user to enter the countdown time in seconds
 t = input('Enter the time in seconds: ')
-
-# Call the countdown function with the user-provided time (converted to an integer)
 countdown(int(t))
